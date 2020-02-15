608a7953423a13219d913d0dd04507de7e783ce2
Peter Shelley <petershe@live.unc.edu>
2020-02-15 14:31:31 -0500

    Customized flashcard app

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -12,13 +12,56 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var optionA: UIButton!
+    @IBOutlet weak var optionB: UIButton!
+    @IBOutlet weak var optionC: UIButton!
+    @IBOutlet weak var correctLabel: UILabel!
+    @IBOutlet weak var incorrectLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        optionA.backgroundColor = #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1)
+        optionB.backgroundColor = #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1)
+        optionC.backgroundColor = #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1)
+        correctLabel.isHidden = true
+        incorrectLabel.isHidden = true
     }
 
     @IBAction func didTapOnFlashcard(_ sender: Any) {
         self.questionLabel.isHidden = !self.questionLabel.isHidden
     }
+    
+    @IBAction func selectOptionA(_ sender: Any) {
+        if optionA.currentTitle==answerLabel.text {
+            questionLabel.isHidden = true
+            correctLabel.isHidden = false
+            incorrectLabel.isHidden = true
+        } else {
+            incorrectLabel.isHidden = false
+            correctLabel.isHidden = true
+        }
+    }
+    
+    @IBAction func selectOptionB(_ sender: Any) {
+        if optionB.currentTitle==answerLabel.text {
+            questionLabel.isHidden = true
+            correctLabel.isHidden = false
+            incorrectLabel.isHidden = true
+        } else {
+            incorrectLabel.isHidden = false
+            correctLabel.isHidden = true
+        }
+    }
+    
+    @IBAction func selectOptionC(_ sender: Any) {
+        if optionC.currentTitle==answerLabel.text {
+            questionLabel.isHidden = true
+            correctLabel.isHidden = false
+            incorrectLabel.isHidden = true
+        } else {
+            incorrectLabel.isHidden = false
+            correctLabel.isHidden = true
+        }
+    }
+    
 }
 
