b9ffdaf9406d75d2b44b7f36884318381f18cdee
Peter Shelley <petershe@live.unc.edu>
2020-03-07 12:39:01 -0500

    You can now change the question and answer on the flashcard.

---
+++ Flashcards/CreationViewController.swift
@@ -0,0 +1,48 @@
+//
+//  CreationViewController.swift
+//  Flashcards
+//
+//  Created by Peter Shelley on 3/3/20.
+//  Copyright © 2020 Peter Shelley. All rights reserved.
+//
+
+import UIKit
+
+class CreationViewController: UIViewController {
+    
+    var flashcardController: ViewController!
+    @IBOutlet var questionText: UITextField!
+    @IBOutlet var answerText: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func didTapOnCancel(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func didTapOnDone(_ sender: Any) {
+        let qText = questionText.text
+        let aText = answerText.text
+        flashcardController.updateFlashcard(question: qText!,
+                                            answer: aText!)
+        dismiss(animated: true)
+    }
+    
+
+    
+    // MARK: - Navigation
+    /*
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        let navigationController = segue.destination as! UINavigationController
+        let creationController = navigationController.topViewController as! CreationViewController
+        CreationViewController.flashcardController = self
+    }
+    */
+
+}
--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -12,58 +12,23 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var questionLabel: UILabel!
-    @IBOutlet weak var optionA: UIButton!
-    @IBOutlet weak var optionB: UIButton!
-    @IBOutlet weak var optionC: UIButton!
-    @IBOutlet weak var correctLabel: UILabel!
-    @IBOutlet weak var incorrectLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        optionA.backgroundColor = #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1)
-        optionB.backgroundColor = #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1)
-        optionC.backgroundColor = #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1)
-        correctLabel.isHidden = true
-        incorrectLabel.isHidden = true
     }
 
     @IBAction func didTapOnFlashcard(_ sender: Any) {
         self.questionLabel.isHidden = !self.questionLabel.isHidden
     }
     
-    @IBAction func selectOptionA(_ sender: Any) {
-        if optionA.currentTitle==answerLabel.text {
-            questionLabel.isHidden = true
-            correctLabel.isHidden = false
-            incorrectLabel.isHidden = true
-        } else {
-            incorrectLabel.isHidden = false
-            correctLabel.isHidden = true
-            questionLabel.isHidden = false
-        }
+    func updateFlashcard(question: String?, answer: String?) {
+        answerLabel.text = answer
+        questionLabel.text = question
     }
     
-    @IBAction func selectOptionB(_ sender: Any) {
-        if optionB.currentTitle==answerLabel.text {
-            questionLabel.isHidden = true
-            correctLabel.isHidden = false
-            incorrectLabel.isHidden = true
-        } else {
-            incorrectLabel.isHidden = false
-            correctLabel.isHidden = true
-            questionLabel.isHidden = false
-        }
-    }
-    
-    @IBAction func selectOptionC(_ sender: Any) {
-        if optionC.currentTitle==answerLabel.text {
-            questionLabel.isHidden = true
-            correctLabel.isHidden = false
-            incorrectLabel.isHidden = true
-        } else {
-            incorrectLabel.isHidden = false
-            correctLabel.isHidden = true
-            questionLabel.isHidden = false
-        }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let navigationController = segue.destination as! UINavigationController
+        let creationController = navigationController.topViewController as! CreationViewController
+        creationController.flashcardController = self
     }
     
 }
